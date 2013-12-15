9e047f675210052a83eceaab612cee1d85dbd378
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-14 21:02:51 -0700

    directive to directive communication (superheroApp)

--- main.js
+++ main.js
@@ -1,15 +1,61 @@
-var app = angular.module("twitterApp", [])
+var app = angular.module("superApp", [])
 
-app.controller("AppCtrl", function($scope) {
-  $scope.loadMoreTweets = function() {
-    alert("Loading tweets!");
-  }
-})
+app.directive("superhero", function() {
+  return {
+    restrict: "E",
+    scope:{},
 
-app.directive("enter", function() {
-  return function(scope, el, attrs) {
-    el.bind("mouseenter", function() {
-      scope.$apply(attrs.enter);
-    })
-  }
-})
+    controller: function($scope) {
+      $scope.abilities = []
+
+      this.addStrength = function() {
+        $scope.abilities.push("strength")
+      }
+
+      this.addSpeed = function() {
+        $scope.abilities.push("speed")
+      }
+
+      this.addFlight = function() {
+        $scope.abilities.push("flight")
+      }
+    },
+
+    link: function(scope, el) {
+      el.addClass("button");
+      el.bind("mouseenter", function() {
+        console.log(scope.abilities);
+      })
+    }
+  };
+});
+
+app.directive('strength', function(){
+  return {
+    require: "superhero",
+
+    link: function(scope, el, attrs, superheroCtrl) {
+      superheroCtrl.addStrength();
+    }
+  };
+});
+
+app.directive('speed', function(){
+  return {
+    require: "superhero",
+
+    link: function(scope, el, attrs, superheroCtrl) {
+      superheroCtrl.addSpeed();
+    }
+  };
+});
+
+app.directive('flight', function(){
+  return {
+    require: "superhero",
+
+    link: function(scope, el, attrs, superheroCtrl) {
+      superheroCtrl.addFlight();
+    }
+  };
+});
