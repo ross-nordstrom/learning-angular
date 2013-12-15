0b35a7edd7c48d3fd4df2d8a34479f1381611151
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-15 08:46:16 -0700

    docs#directive: dragme example

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
-var app = angular.module("app", []);
+angular.module('dragModule', []).
+  directive('myDraggable', function($document) {
+    return function(scope, element, attr) {
+      var startX = 0, startY = 0, x = 0, y = 0;
 
-app.directive("country", function() {
-  return {
-    restrict: "E",
-    controller: function() {
-      this.makeAnnouncement = function(message) {
-        console.log("Country says: "+message);
+      element.css({
+       position: 'relative',
+       border: '1px solid red',
+       backgroundColor: 'lightgrey',
+       cursor: 'pointer'
+      });
+
+      element.on('mousedown', function(event) {
+        // Prevent default dragging of selected content
+        event.preventDefault();
+        startX = event.pageX - x;
+        startY = event.pageY - y;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+      });
+
+      function mousemove(event) {
+        y = event.pageY - startY;
+        x = event.pageX - startX;
+        element.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+
+      function mouseup() {
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('mouseup', mouseup);
       }
     }
-  };
-});
-
-app.directive("state", function () {
-  return {
-    restrict: "E",
-    controller: function () {
-      this.makeLaw = function (law) {
-        console.log("Law: " + law);
-      };
-    }
-  };
-});
-
-app.directive("city", function () {
-  return {
-    restrict: "E",
-    require: ["^country","^state"],
-    link: function (scope, element, attrs, ctrls) {
-      ctrls[0].makeAnnouncement("This city rocks");
-      ctrls[1].makeLaw("Jump higher");
-    }
-  };
-});
+  });
