04a28557b93cf1d49d765585d56c81596a856711
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-15 09:24:26 -0700

    egghead: isolate scope '@'

--- main.js
+++ main.js
@@ -1,36 +1,13 @@
-angular.module('dragModule', []).
-  directive('myDraggable', function($document) {
-    return function(scope, element, attr) {
-      var startX = 0, startY = 0, x = 0, y = 0;
+angular.module("drinkApp", [])
+  .controller("AppCtrl", function($scope) {
+    $scope.ctrlFlavor = "blackberry"
 
-      element.css({
-       position: 'relative',
-       border: '1px solid red',
-       backgroundColor: 'lightgrey',
-       cursor: 'pointer'
-      });
-
-      element.on('mousedown', function(event) {
-        // Prevent default dragging of selected content
-        event.preventDefault();
-        startX = event.pageX - x;
-        startY = event.pageY - y;
-        $document.on('mousemove', mousemove);
-        $document.on('mouseup', mouseup);
-      });
-
-      function mousemove(event) {
-        y = event.pageY - startY;
-        x = event.pageX - startX;
-        element.css({
-          top: y + 'px',
-          left:  x + 'px'
-        });
-      }
-
-      function mouseup() {
-        $document.unbind('mousemove', mousemove);
-        $document.unbind('mouseup', mouseup);
-      }
+  })
+  .directive("drink", function() {
+    return {
+      scope: {
+        flavor: "@"
+      },
+      template: '<div>{{flavor}}</div>'
     }
-  });
+  })
