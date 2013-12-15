cb0203592346c8d7e4a5c2c37b66cfef5e483f97
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-15 10:58:29 -0700

    use scope.() to cleanup a scope; in this example, only let the event counters hit 10, then destroy that scope

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ angular.module("eventApp", [])
     $scope.count = 0;
     $scope.$on('MyEvent', function() {
       $scope.count++;
+      if($scope.count > 10) {
+        $scope.$destroy();
+      }
     })
   })
