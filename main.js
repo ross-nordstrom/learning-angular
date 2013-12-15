ab5d35376fabe561f928b6f0064d7977ef892d11
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-14 21:07:57 -0700

    phoneApp => transclusion

--- main.js
+++ main.js
@@ -1,61 +1,14 @@
-var app = angular.module("superApp", [])
+var app = angular.module("phoneApp", []);
 
-app.directive("superhero", function() {
+app.controller("AppCtrl", function($scope) {
+
+
+});
+
+app.directive("panel", function() {
   return {
     restrict: "E",
-    scope:{},
-
-    controller: function($scope) {
-      $scope.abilities = []
-
-      this.addStrength = function() {
-        $scope.abilities.push("strength")
-      }
-
-      this.addSpeed = function() {
-        $scope.abilities.push("speed")
-      }
-
-      this.addFlight = function() {
-        $scope.abilities.push("flight")
-      }
-    },
-
-    link: function(scope, el) {
-      el.addClass("button");
-      el.bind("mouseenter", function() {
-        console.log(scope.abilities);
-      })
-    }
-  };
-});
-
-app.directive('strength', function(){
-  return {
-    require: "superhero",
-
-    link: function(scope, el, attrs, superheroCtrl) {
-      superheroCtrl.addStrength();
-    }
-  };
-});
-
-app.directive('speed', function(){
-  return {
-    require: "superhero",
-
-    link: function(scope, el, attrs, superheroCtrl) {
-      superheroCtrl.addSpeed();
-    }
-  };
-});
-
-app.directive('flight', function(){
-  return {
-    require: "superhero",
-
-    link: function(scope, el, attrs, superheroCtrl) {
-      superheroCtrl.addFlight();
-    }
-  };
-});
+    transclude: true,
+    template: '<div class="panel" ng-transclude>This is a panel component</div>'
+  }
+})
