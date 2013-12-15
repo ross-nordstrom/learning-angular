92533d5e3e39f3f6fe751b58aac600d6ce811be9
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-15 10:51:43 -0700

    scope events propagation

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
-angular.module("phoneApp", [])
-  .controller('AppCtrl', function($scope){
-    $scope.leaveVoicemail = function(number, message){
-      alert('Number: ' + number + ' said: ' + message);
-    };
+angular.module("eventApp", [])
+  .controller('EventController', function($scope) {
+    $scope.count = 0;
+    $scope.$on('MyEvent', function() {
+      $scope.count++;
+    })
   })
-
-  .directive('phone', function(){
-    return {
-      restrict: 'E',
-      scope: {
-        number: '@',
-        network: '=',
-        makeCall: '&'
-      },
-      template: '<div>'+
-          'Number: {{number}}'+
-          'Network:<select ng-model="network" ng-options="net for net in networks"></select>'+
-        '</div>'+
-        '<input type="text" ng-model="value">'+
-        '<div class="button" ng-click="makeCall({message: value, number: number})">'+
-          'Call Home!'+
-        '</div>',
-      link: function(scope){
-          scope.networks = ["Verizon", "AT&T", "Sprint"];
-          scope.network = scope.networks[0];
-        }
-    };
-  });
