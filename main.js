f2b7c3bb6dacbfd65734ce3919127e735cd6478c
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-15 11:52:10 -0700

    egghead: angular.element - an example of editing elements in the view (dumd-password)

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
-angular.module('xmpl.service', []).
-  value('greeter', {
-    salutation: 'Hello',
-    localize: function(localization) {
-      this.salutation = localization.salutation;
-    },
-    greet: function(name) {
-      return this.salutation + ' ' + name + '!';
+var app = angular.module("app", [])
+
+app.directive("dumbPassword", function () {
+  var validElement = angular.element('<div>{{ model.input }}</div>');
+
+  var link = function(scope) {
+    scope.$watch("model.input", function(value) {
+      if(value === 'password') {
+        validElement.addClass("alert-box alert")
+      } else {
+        validElement.removeClass("alert-box alert")
+      }
+    })
+  }
+
+  return {
+    restrict: "E",
+    replace:true,
+    template: '<div>'+
+        '<input type="text" ng-model="model.input">'+
+      '</div>',
+    compile: function(tElem) {
+      tElem.append(validElement);
+
+      return link;
     }
-  }).
-  value('user', {
-    load: function(name) {
-      this.name = name;
-    }
-  });
-
-angular.module('xmpl.directive', []);
-
-angular.module('xmpl.filter', []);
-
-angular.module('xmpl', ['xmpl.service', 'xmpl.directive', 'xmpl.filter']).
-  run(function(greeter, user) {
-    // This is effectively part of the main method initialization code
-    greeter.localize({
-      salutation: 'Bonjour'
-    });
-    user.load('World');
-  })
-
-
-// A Controller for your app
-var XmplController = function($scope, greeter, user) {
-  $scope.greeting = greeter.greet(user.name);
-}
+  };
+});
