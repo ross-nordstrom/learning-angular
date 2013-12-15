764fda7809d1c3a2c8ff36e37b0e9d5c1e0962b5
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-15 12:03:56 -0700

    egghead: , ,  -- example of using  and  to configure loglevel-based console printouts

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
-var app = angular.module("app", [])
+var app = angular.module("app", []);
 
-app.directive("dumbPassword", function () {
-  var validElement = angular.element('<div>{{ model.input }}</div>');
+app.config(function($logProvider){
+  $logProvider.debugEnabled(false);
+});
 
-  var link = function(scope) {
-    scope.$watch("model.input", function(value) {
-      if(value === 'password') {
-        validElement.addClass("alert-box alert")
-      } else {
-        validElement.removeClass("alert-box alert")
-      }
-    })
-  }
-
-  return {
-    restrict: "E",
-    replace:true,
-    template: '<div>'+
-        '<input type="text" ng-model="model.input">'+
-      '</div>',
-    compile: function(tElem) {
-      tElem.append(validElement);
-
-      return link;
-    }
-  };
+app.run(function($rootScope, $log){
+  $rootScope.$log = $log;
 });
