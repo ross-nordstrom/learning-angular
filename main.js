98bd5af126e88e1231ba93ca710b446c18287503
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-15 22:34:33 -0700

    route parameters: use [#/map/foo/bar/baz](file:///home/ross/Development/personal/learning-angular/index.html#/map/foo/bar/baz)

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 var app = angular.module('app', ['ngRoute']);
 
-app.config(function($routeProvider){
-  $routeProvider.when("/",
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/:map/:country/:state/:city',
     {
       templateUrl: "app.html",
       controller: "AppCtrl"
     }
-  );
+  )
 });
 
-app.controller("AppCtrl", function($scope){
+app.controller("AppCtrl", function ($scope, $routeParams) {
   $scope.model = {
-    message: "This is my app!!!"
+    message: "Address: " +
+      $routeParams.country + ", " +
+      $routeParams.state + ", " +
+      $routeParams.city
   }
 });
