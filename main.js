38d6d63ae3ea017948651fd58167ffb57e2e93d8
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-15 22:55:47 -0700

    resolve after a timeout

--- main.js
+++ main.js
@@ -4,29 +4,22 @@ app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: "app.html",
-      controller: "AppCtrl"
+      controller: "AppCtrl",
+      resolve: {
+        app: function($q, $timeout) {
+          var defer = $q.defer();
+          $timeout(function() {
+            defer.resolve();
+          }, 2000);
+          return defer.promise
+        }
+      }
     })
 });
 
 app.controller("AppCtrl", function ($scope, $q) {
-  var defer = $q.defer();
-
-  defer.promise
-    .then(function(weapon) {
-      alert("You can have my "+weapon)
-      return "bow"
-    })
-    .then(function(weapon) {
-      alert("and my "+weapon)
-      return "axe"
-    })
-    .then(function(weapon) {
-      alert("and also my "+weapon)
-    });
 
   $scope.model = {
     message: "This is my app!!!"
   }
-
-  defer.resolve("sword");
 });
