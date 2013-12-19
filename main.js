6c01c14d19d28ee0e5f0b1cecc9fdfc2448cee85
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-18 22:14:46 -0700

    egghead: zippy app

--- main.js
+++ main.js
@@ -1,46 +1,19 @@
 var app = angular.module('app', ['ngRoute']);
 
-app.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: "app.html",
-      controller: "ViewCtrl",
-      resolve: {
-        loadData: viewCtrl.loadData
-      }
-    })
-});
-
-app.directive("error", function($rootScope) {
+app.directive("zippy", function() {
   return {
     restrict: "E",
-    template: '<div class="alert-box alert" ng-show="isError">Error: {{rejection}}</div>',
+    transclude:true,
+    scope:{
+      title:"@"
+    },
+    template: '<div><h3 ng-click="toggleContent()">{{title}}</h3>'+
+              '<div ng-show="isContentVisible" ng-transclude>Hello world</div></div>',
     link: function(scope) {
-      $rootScope.$on("$routeChangeError", function(event, current, previous, rejection) {
-        scope.isError = true;
-        scope.rejection = rejection;
-      })
+      scope.isContentVisible = false;
+      scope.toggleContent = function() {
+        scope.isContentVisible = !scope.isContentVisible;
+      }
     }
   }
 })
-
-app.controller("AppCtrl", function($rootScope) {
-  $rootScope.$on("$routeChangeError", function(event, current, previous, rejection) {
-    console.log(rejection);
-  })
-})
-
-var viewCtrl = app.controller("ViewCtrl", function ($scope, $route) {
-  console.log($route);
-  $scope.model = {
-    message: "This is my app!!!"
-  }
-});
-
-viewCtrl.loadData = function($q, $timeout) {
-  var defer = $q.defer();
-  $timeout(function() {
-    defer.reject("Your network is down");
-  }, 2000);
-  return defer.promise
-}
